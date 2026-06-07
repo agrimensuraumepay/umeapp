c10bdeae763b9273125144ec06c62875305d5a77
agrimensuraumepay <agrimensuraumepay@gmail.com>
2026-06-07 20:00:38 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'umeapp-v1';
+const CORE = [
+  './', './index.html', './mapa.html', './cartelera.html', './gas.html',
+  './manifest.json', './icon-192.png', './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;            // no tocar los envíos (pedidos de gas, etc.)
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;  // CDN, mapas y hojas van directo a la red
+  e.respondWith(
+    fetch(req).then(res => {
+      const copia = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copia));
+      return res;
+    }).catch(() => caches.match(req))
+  );
+});
